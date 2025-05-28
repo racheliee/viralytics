025e83d8c931189e31d7437d4f99f80134b1751a
racheliee <rachelie.dwg@gmail.com>
2025-05-28 22:57:13 +0900

    config

--- ui/src/lib/config.ts
+++ ui/src/lib/config.ts
@@ -1,5 +1,5 @@
 export const config = {
   apiBaseUrl: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3002',
-  environment: process.env.NODE_ENV || 'development',
+  env: process.env.NODE_ENV || 'development',
   frontendUrl: process.env.NEXT_PUBLIC_FRONTEND_URL || 'http://localhost:3000'
 }
